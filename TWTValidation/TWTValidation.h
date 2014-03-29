d15298ce708eaac0a6d75d2cd6a5e5ae6855ff9b
Prachi Gauriar <prachi@twotoasters.com>
2014-03-28 22:09:20 -0400

    Initial Commit

---
+++ TWTValidation/TWTValidation.h
@@ -0,0 +1,13 @@
+//
+//  TWTValidation.h
+//  TWTValidation
+//
+//  Created by Prachi Gauriar on 3/28/2014.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TWTValidation : NSObject
+
+@end
